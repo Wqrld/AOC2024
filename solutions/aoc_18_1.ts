5546401788c087732254fbfa5d76fbc3d55b66b3
Luc H <wereld03@gmail.com>
2024-12-18 10:41:29 +0100

    implement a binary search for day 18 part 2 to speed up searching

--- solutions/aoc_18_1.ts
+++ solutions/aoc_18_1.ts
@@ -2,39 +2,72 @@ const fs = require("fs");
 
 let input = fs.readFileSync("inputs_prod/18.txt", "utf8").split("\n").map((line) => line.split(",").map(Number));
 
-let field = new Array(71).fill(".").map(() => new Array(71).fill("."));
+const offsets = [[0, -1], [1, 0], [0, 1], [-1, 0]];
+// find x, y positions of Start and End
+const [startX, startY] = [0, 0]
 
-// console.log(field);
+const [endX, endY] = [70, 70]
 
-// find x, y positions of S and E symbols in field
-let [startX, startY] = [0, 0]
-
-let [endX, endY] = [70, 70]
-
-// field[startY][startX] = ".";
+function attemptEscape(iterations) {
 
 
+    let field = new Array(71).fill(".").map(() => new Array(71).fill("."));
 
-const graph = {}
+    const graph = {}
 
-for (let i = 0; i < input.length && i < 1024; i++) {
-    const [x, y] = input[i];
-    field[y][x] = "#";
+    for (let i = 0; i < input.length && i < iterations; i++) {
+        const [x, y] = input[i];
+        field[y][x] = "#";
+    }
+
+    // drawField(field);
+
+    //todo optimization: we can get away with not rebuilding the graph every time
+    // instead, we can loop over all vertices and delete the paths to the blocked vertex
+    buildGraph(graph, field, [startX, startY]);
+    console.log("Graph built with", Object.keys(graph).length, "vertices");
+    if (!graph[`${endX},${endY}`]) {
+        drawField(field)
+        console.log("No path to end")
+        console.log("Problematic block", input[iterations - 1]);
+        return 0;
+    }
+
+    graph['start'] = {};
+    graph['start'][`${startX},${startY}`] = 1;
+    graph[`${endX},${endY}`]['end'] = 1;
+    graph['end'] = {};
+
+    const { distance, path } = dijkstraAlgorithm(graph)
+    // console.log(path)
+    // console.log(distance - 2)
+    return distance - 2;
 }
 
-drawField(field);
+// Linear search
+// for (let i = 1024; i < 10000; i++) {
+//     let attempt = attemptEscape(i);
+//     if (attempt == 0) break;
+//     console.log(i, attemptEscape(i))
+// }
 
-const offsets = [[0, -1], [1, 0], [0, 1], [-1, 0]];
+// try a binary search
+let Low = 1024;
+let High = input.length;
+let Mid = Math.floor((Low + High) / 2);
+while (High - Low > 1) {
+    let attempt = attemptEscape(Mid);
+    console.log(High, Low, Mid, attempt)
+    if (attempt == 0) {
+        // our guess is too high
+        High = Mid;
+    } else {
+        // our guess is too low
+        Low = Mid;
+    }
+    Mid = Math.floor((Low + High) / 2);
+}
 
-buildGraph(field, [startX, startY]);
-console.log("Graph built with", Object.keys(graph).length, "vertices");
-
-// console.log(graph)
-
-graph['start'] = {};
-graph['start'][`${startX},${startY}`] = 1;
-graph[`${endX},${endY}`]['end'] = 1;
-graph['end'] = {};
 
 function drawField(field) {
     for (let row of field) {
@@ -47,7 +80,7 @@ function drawField(field) {
 // next to dijkstra, we may use lee algorithm or A*
 
 
-function buildGraph(field, [x, y]) {
+function buildGraph(graph, field, [x, y]) {
     if (graph[`${x},${y}`]) return;
     // console.log(x, y)
     graph[`${x},${y}`] = {}
@@ -56,7 +89,7 @@ function buildGraph(field, [x, y]) {
         const [newX, newY] = [x + dx, y + dy];
         if (field[newY]?.[newX] === ".") {
             graph[`${x},${y}`][`${newX},${newY}`] = 1;
-            buildGraph(field, [newX, newY]);
+            buildGraph(graph, field, [newX, newY]);
         }
     }
 
@@ -84,7 +117,7 @@ function dijkstraAlgorithm(graph) {
         }
         delete unprocessedcosts[node];
 
-        console.log("Processed", Object.keys(unprocessedcosts).length, "vertices");
+        // console.log("Processed", Object.keys(unprocessedcosts).length, "vertices");
         if (Object.keys(unprocessedcosts).length == 245) console.log(node)
         node = findLowestCostNode(unprocessedcosts);
     }
@@ -104,6 +137,3 @@ function findLowestCostNode(unprocessedcosts) {
     return Object.keys(unprocessedcosts).reduce((acc, node) => unprocessedcosts[node] < unprocessedcosts[acc] ? node : acc, "end");
 };
 
-const { distance, path } = dijkstraAlgorithm(graph)
-console.log(path)
-console.log(distance - 2)
